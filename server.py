e0d55994b9304df4ed1d3d0222f91b00eb558000
kanatanagano <natakana2525@gmail.com>
2023-11-28 15:15:34 +0900

    クラスでメッセージを管理させました

--- server.py
+++ server.py
@@ -1,35 +1,53 @@
 import socket
-import os
+import threading
+import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = '/socket_file'
+class ChatServer:
+    def __init__(self, host, port):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind((host, port))
+        self.clients = {}
 
-# server address unlinked if already exists
-try:
-    os.unlink(server_address)
-except FileExistsError:
-    pass
+    def handle_client(self, message, client_address):
+        username_len = message[0]
+        username = message[1 : username_len + 1].decode('utf-8')
+        user_message = message[username_len + 1 :].decode('utf-8')
 
-print('starting up on {}'.format(server_address))
-sock.bind(server_address)
-sock.listen(1)
+        if client_address not in self.clients:
+            self.clients[client_address] = {
+                "username": username,
+                "last_activity": time.time()
+            }
 
-while True:
-    connection, client_address = sock.accept()
-    try:
-        print('connection from {}'.format(client_address))
+        print(f"Received message from {username}: {user_message}")
+
+        # Update last activity time
+        self.clients[client_address]["last_activity"] = time.time()
+
+        # Relay the message to all connected clients
+        for addr in self.clients:
+            if addr != client_address:
+                self.server_socket.sendto(message, addr)
+
+    def cleanup_inactive_clients(self):
+        # Remove inactive clients
+        current_time = time.time()
+        inactive_clients = [addr for addr, info in self.clients.items() if current_time - info["last_activity"] > 60]  # 60 seconds inactivity timeout
+
+        for addr in inactive_clients:
+            del self.clients[addr]
+            print(f"Client {self.clients[addr]['username']} at {addr} has been removed due to inactivity.")
+
+    def start(self):
+        print("Chat server is running...")
         while True:
-            data = connection.recv(16)
-            data_str = data.decode('utf-8')
-            print('received {}'.format(data_str))
+            message, client_address = self.server_socket.recvfrom(4096)
+            threading.Thread(target=self.handle_client, args=(message, client_address)).start()
 
-            if data:
-                response = 'processing' + data_str
-                connection.sendall(response.encode('utf-8'))
-            else:
-                print('no data from {}'.format(client_address))
-                break
-    finally:
-        print('closing connection')
-        connection.close()
+            # Clean up inactive clients
+            self.cleanup_inactive_clients()
+
+if __name__ == "__main__":
+    server = ChatServer('127.0.0.1', 12345)
+    server.start()
 
